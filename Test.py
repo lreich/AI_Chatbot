a12985bae34f523c901e8c995d68c3edc261687a
Lukas Reich <lukasreich@online.de>
2018-05-17 21:42:08 +0200

    Paul
    guck mal ob die recursive funktion bei dir geht.
    ich krige da "unresolved reference.."
    sollte aber eigl so funktionieren

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 class Test:
-    a = 1
-    b = 1
+    a = 2
+    b = 2
+    c = "2"
+    d  = "eins"
 
     def addieren(x,y):
         return x + y
@@ -8,11 +10,30 @@ class Test:
     def subtrahieren(x,y):
         return x - y
 
-    print(addieren(a,b))
-    print(subtrahieren(a,b))
+    # print(addieren(a,b))
+    # print(subtrahieren(a,b))
 
     def multi(x,y):
         return x * y
 
-    print(addieren(a,b))
-    print (multi(a,b))
+    # print(addieren(a,b))
+    # print (multi(a,b))
+    # print(type(a))
+    # print(int(c))
+
+    # print("enter pls")
+    # u = input()
+    # print("Hello" + u)
+    # print(11 / 2)
+
+    # def calc_factorial(x):
+    #     """This is a recursive function
+    #     to find the factorial of an integer"""
+    #
+    #     if x == 1:
+    #         return 1
+    #     else:
+    #         return (x * calc_factorial(x - 1))
+
+    # num = 4
+    # print("The factorial of", num, "is", calc_factorial(num))
---
+++ condatest.py
@@ -0,0 +1,5 @@
+import tensorflow
+
+
+
+print("hello")
